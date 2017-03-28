873b10dcd7a06e0a0e3c92fedb1ef71f4b708d44
theClubhou.se <heythere@theclubhou.se>
2017-03-28 16:21:38 -0400

    slideshow

--- app/containers/Home/index.js
+++ app/containers/Home/index.js
@@ -6,14 +6,96 @@
 
 import React from 'react';
 import Paper from 'material-ui/Paper';
+import IconButton from 'material-ui/IconButton';
+import ChevLeft from 'material-ui/svg-icons/navigation/chevron-left';
+import ChevRight from 'material-ui/svg-icons/navigation/chevron-right';
 import {Link} from 'react-router';
 import Helmet from 'react-helmet';
 
 export default class Home extends React.PureComponent {
+  constructor(props){
+    super(props);
+    this.state={
+      data:[
+        {
+          articleImage:"https://scontent-atl3-1.xx.fbcdn.net/v/t1.0-9/17264794_273053533135955_2235100976553120977_n.jpg?oh=8a34eb015d5191d4c4933d78b4f137a5&oe=596F03E4",
+          articleTitle:"Infrastructure",
+          articleBody:"Round-a-bout construction on Wheeler @ Aumond"
+        },
+        {
+          articleImage:"https://scontent-atl3-1.xx.fbcdn.net/v/t31.0-8/17390681_1061725620599626_6413932256641965317_o.jpg?oh=71be93b4f5deb33ad3fd2266720b661c&oe=596DFB93",
+          articleTitle:"Jam",
+          articleBody:"4th Annual Major Rager april, 4th"
+        },
+        {
+          articleImage:"https://scontent-atl3-1.xx.fbcdn.net/v/t31.0-8/16797120_1787054604890866_5798852822789312024_o.jpg?oh=cc17569128c9e869d241e4a4d2b94519&oe=59623C06",
+          articleTitle:"The Cyber Train",
+          articleBody:"Choo Choo! C'mon y'all. Augusta is fastly chuggin' to becoming the cyber capital of the nation!",
+        }
+      ],
+      active:0,
+      activeArticle:""
+
+    }
+  }
+  componentWillMount(){
+    this.setState({
+      activeArticle:this.state.data[0]
+    })
+  }
+  nextArticle = () =>{
+    var active=this.state.active;
+    var next = active+1;
+    if(next > this.state.data.length) {
+      next = 0;
+    }
+
+    for(var i = 0; i<this.state.data.length; i++){
+      if(i == next){
+        this.setState({
+          activeArticle:this.state.data[i]
+        })
+      }
+    }
+
+    this.setState({
+      active:next
+    })
+  }
+  previousArticle =() =>{
+    var active=this.state.active;
+    var prev = active-1;
+    if(prev < 0) {
+      prev = this.state.data.length;
+    }
+    for(var i = 0; i<this.state.data.length; i++){
+      if(i == prev){
+        this.setState({
+          activeArticle:this.state.data[i]
+        })
+      }
+    }
+    this.setState({
+      active:prev
+    })
+  }
+
   render() {
-const container={
+const imageOverlay={
+        background:"rgba(0,0,0,0.2)",
+        position:"absolute",
+        top:"0",
+        width:"100%",
+        height:"100%",
+        display:"flex",
+        justifyContent:"space-between",
+        alignItems:"center",
+
+    }
+  const container={
   display:"flex",
   flexDirection:"row",
+
 }
 
   const navColumn={
@@ -25,28 +107,31 @@ const container={
 
 
 
+
     }
       const logo={
         width:"90%",
-        height:"10px",
+        height:"10%",
+
+
       }
         const wuzGudLogo={
           fontFamily:"Modak",
-          color:"#CE4632",
+          color:"#E6BE2B",
           fontSize:"5em",
           textAlign:"center",
           marginTop:"30px",
-          textShadow:"4.3px 1.5px .3px #E6BE2B",
+          textShadow:"4.3px 1.5px .3px #CE4632",
 
         }
       const leftNav={
-        width:"99%",
-        height:"400px",
-        marginTop:"30px",
+        width:"90%",
+        height:"auto",
+        marginTop:"10%",
         borderTop:"2px solid #2D2C2C",
         borderBottom:"2px solid #2D2C2C",
         textAlign:"center",
-        lineHeight:"65px",
+        lineHeight:"3.25em",
         color:"#818286",
         fontFamily:"Amiko",
         fontSize:"1.25em"
@@ -54,24 +139,33 @@ const container={
       }
         const navLink={
           fontFamily:"Amiko",
-          fontSize:"1.25em",
+          fontSize:".8em",
           color:"#818286",
           textDecoration:"none",
+          fontWeight:"bold",
         }
     const contentFeild={
-      width:"65%",
+      width:"55%",
       height:"100vh",
       background:"#005188",
     }
 
-      const headLine={
-        width:"90%",
-        height:"500px",
-        margin:"50px",
-        marginBottom:"5px",
-        background:"#005188",
-        textAlign:"center",
-        borderBottom:"2px solid #2D2C2C",
+
+      const boxStyle={
+        width:"85%",
+        height:"auto",
+        display:"flex",
+        flexDirection:"grid",
+        margin:"30px",
+        position:"relative",
+
+
+      }
+      const boxImage={
+        width:"100%",
+        height:"80%",
+        margin:"0 auto",
+
       }
       const paper={
         height: "100vh",
@@ -111,7 +205,16 @@ const container={
           </div>
         </div>
         <div style={contentFeild}>
-          <div style={headLine}>
+          <div>
+
+            <div style={boxStyle}>
+              <img style={boxImage} src={this.state.activeArticle.articleImage}/>
+                <div style={imageOverlay}>
+                  <IconButton onTouchTap={this.previousArticle}><ChevLeft color="#ffffff"/></IconButton>
+                  <IconButton onTouchTap={this.nextArticle}><ChevRight color="#ffffff"/></IconButton>
+                </div>
+            </div>
+
           </div>
 
             <div style={content}>
